d1be9dd8fd182fc6d47732671204de0ecaae8a37
Ilya Noskov <VishiiTamplier@yandex.ru>
2021-11-08 22:48:12 +0300

    new mcmc

--- mcmc.py
+++ mcmc.py
@@ -17,9 +17,10 @@ class MCMC():
     def __init__(self, beta,gamma, warmup,yes,*args, **kwargs):
         self.beta = beta
         self.gamma = gamma
-        self.model = SIR.SIR(1000,1,100,beta=self.beta,gamma=self.gamma,dt=4)
+        self.model = SIR(1000,1,100,beta=self.beta,gamma=self.gamma,dt=4)
         self.x = [self.beta, self.gamma]
         self.yes = yes
+        self.reject=[]
     def log_likelihood(self,x,y):
         return tfp.distributions.Poisson(x).log_prob(y)
     def sum_log_likelihood(self,xes,yes):
@@ -29,15 +30,15 @@ class MCMC():
 
         return np.sum( [self.log_likelihood (xes[t], yes[t]) for t in range (0, len(xes) ) ] )
     def proposal(self,x):        
-        return [ tfp.distributions.HalfNormal (mu,0.1).sample().numpy() for mu in x]
+        return [ abs(tfp.distributions.Normal(mu,0.001).sample().numpy()) for mu in x]
     def run(self,iter):
         self.result = []
         for i in range(0,iter):
             x_new = self.proposal(self.x)
-            self.model = SIR.SIR(1000,1,100,beta = self.x[0],gamma = self.x[1],dt=4)
+            self.model = SIR(1000,1,100,beta = self.x[0],gamma = self.x[1],dt=4)
             data_old = self.model.get_results()[:10]
             
-            self.model = SIR.SIR(1000,1,100,beta=x_new[0],gamma=x_new[1],dt=4)
+            self.model = SIR(1000,1,100,beta=x_new[0],gamma=x_new[1],dt=4)
             
             data_new = self.model.get_results()[:10]
             
@@ -47,4 +48,12 @@ class MCMC():
             if log_lik_x_new > log_lik_x:
                 self.x = x_new
                 self.result.append(x_new)
-                print(log_lik_x,log_lik_x_new)
+                #print(log_lik_x,log_lik_x_new)
+            else:
+                alpha=tfp.distributions.Uniform(0.,1.).sample().numpy()
+                #print( np.log(alpha),log_lik_x_new-log_lik_x)
+                if  np.log(alpha )< ((log_lik_x_new-log_lik_x)):
+                    self.x = x_new
+                    self.result.append(x_new)
+                else:
+                    self.result.append(self.x)
