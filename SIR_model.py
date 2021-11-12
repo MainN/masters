ad2d1458a5c79ad863e3d13657acd676f19a7d95
DESKTOP-D5KOAQA\Mikhail <2pi360@gmail.com>
2021-11-12 23:03:34 +0200

    some style to SIR_model

--- SIR_model.py
+++ SIR_model.py
@@ -10,6 +10,8 @@ import matplotlib.animation as animation
 from matplotlib.animation import PillowWriter
 from matplotlib.animation import FuncAnimation
 sns.set()
+
+
 class SIR():
     def __init__(self,size,percent,t,*args, beta, gamma, dt,**kwargs):
     #инициализируем изначальное состояние модели
@@ -25,35 +27,35 @@ class SIR():
         self.dt = dt
         self.t = t
         self.steps = round(t/dt)
-        
+
     def calc(self):
-        #вычисление 
+        #вычисление
         for t in range(0,self.steps):
             #считаем приращения
             ds_dt=self.dS_dt()
             di_dt=self.dI_dt()
             dr_dt=self.dR_dt()
-            
+
             #добавляем в результирующие списки
-            self.result_S.append(ds_dt )
-            self.result_I.append(di_dt )
-            self.result_R.append(dr_dt )
-            
+            self.result_S.append(ds_dt)
+            self.result_I.append(di_dt)
+            self.result_R.append(dr_dt)
+
             #пересчитываем количество людей в группах
             self.S += ds_dt
             self.I += di_dt
             self.R += dr_dt
-          
+
         #выводим количество индивидуумов в каждой группе
         #print(self.S,self.I,self.R)
-        
+
     def dS_dt(self):
         return (-self.beta * self.S * self.I / self.N) * self.dt
     def dR_dt(self):
         return self.gamma * self.I*self.dt
     def dI_dt(self):
         return (self.beta * self.S * self.I / self.N - self.gamma * self.I)*self.dt
-        
+
     def viz(self):
         #Собираем в табличку полученные списки прирощений
         table=list(zip(self.result_S,self.result_I,self.result_R))
@@ -62,19 +64,20 @@ class SIR():
                columns =['S', 'I','R'])
         #возвращаем визуализацию
         return (sns.lineplot(data=df))
-    def set_params(self,params):
-        self.beta=params[0]
-        self.gamma=params[1]
-        
+
+    def set_params(self, params):
+        self.beta = params[0]
+        self.gamma = params[1]
         #print(self.beta,self.gamma)
+
     def get_results(self,xes):
-        
         self.calc()
         self.result_S = self.normalize(xes)
         #print((self.beta,self.gamma),self.result_S)
         return [abs(x) for x in self.result_S]
     def get_R0(self):
         return self.beta/self.gamma
+
     def normalize(self,lst):
         pos=-1
         for x in range(0,len(self.result_S)):
